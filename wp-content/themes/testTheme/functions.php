b88ba1cbbabffa360a3394c5fa616f6b78afe870
jobcrackerbah <erik@critiqueit.com>
2016-01-27 01:08:22 -0800

    Adds functions.php code

---
+++ wp-content/themes/testTheme/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+// Holds wordpress custom scripts to be included.  Declare function then run in add action
+function test_enqueue() {
+	wp_enqueue_style('customStyleId', get_template_directory_uri() . '/css/test.css', array(), '1.0.0', 'all');
+}
+
+// hook that gives connection wordpress execution process to a custom function.  Gets triggered by wp_head();
+add_action('wp_enqueue_scripts', 'test_enqueue');
+
+// activitate specific theme support with a hook.  Easier to use functions because easy to keep track
+function test_theme_setup(){
+	add_theme_support( 'custom-header' );
+	add_theme_support('menus');
+	// description registering our nav menu hook
+	register_nav_menu('main', 'Main Header Navigation');
+	
+	
+};
+
+// use init because adding more functions to test them setup
+add_action('init', 'test_theme_setup');
+
+?>
