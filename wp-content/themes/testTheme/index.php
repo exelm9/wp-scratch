55a8e0aa0811f5aebfe288ba7c93c5e0b30997d6
jobcrackerbah <erik@critiqueit.com>
2016-01-27 01:22:08 -0800

    Adds titles to the top all pages

--- wp-content/themes/testTheme/index.php
+++ wp-content/themes/testTheme/index.php
@@ -2,6 +2,10 @@
 
 	<?php if( have_posts() ): ?>
 
+			<h1 class="page-title screen-reader-text">
+				<?php single_post_title(); ?>
+			</h1>
+			
 		<?php if ( is_home() && ! is_front_page() ) : ?>
 			
 		<?php endif; ?>
