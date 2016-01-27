646c0f493a0a635d5da560cbefe003ff2e3cda42
jobcrackerbah <erik@critiqueit.com>
2016-01-27 01:11:07 -0800

    Adds footer and header code

---
+++ wp-content/themes/testTheme/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Test Theme</title>
+	<!-- connects wordpress hooks -->
+		<?php wp_head(); ?>
+	
+</head>
+
+<body <?php body_class(); ?>>
+
+	
+</header>
